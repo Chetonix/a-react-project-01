93d09c01883d4e9d22abe6bcb29a4360e705ff77
Chetan Harjani <chetonix@Chetans-MacBook-Pro.local>
2022-06-28 17:39:09 +0530

    add codebase to the repo

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from './Card';
+
+export default function ExpenseItem(props) {
+
+    // const expenseDate = new Date().toISOString();
+    // const expenseTitle = "Car Insurance";
+    // const expenseAmount = 294.56;
+
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    
+    );
+}
