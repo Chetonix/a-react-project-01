30eac1829a93c8b663f7782501c82d156ccad37b
Chetan Harjani <chetonix@Chetans-MacBook-Pro.local>
2022-06-29 13:54:47 +0530

    add NewExpense component in the codebase

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
@@ -7,14 +8,21 @@ export default function ExpenseItem(props) {
     // const expenseDate = new Date().toISOString();
     // const expenseTitle = "Car Insurance";
     // const expenseAmount = 294.56;
-
+    const [title, setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluated by React');
+  
+    const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+    };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
     
